f5092906d0d313247a2875a15622c35cf07fc76a
naimurcse <2015naim@gmail.com>
2023-11-12 21:11:21 +0600

    CSS applied in Users, User and App Components

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,12 +1,17 @@
 import React from "react";
+import "./User.css";
 
 const User = (props) => {
    const { id, name, address, picture, balance } = props.user;
    console.log(props.user);
    return (
-      <div>
-         <h2>{name}</h2>
-         <h5>Price: {balance}</h5>
+      <div className="user-cart">
+         <h2 className="user-title">{name}</h2>
+         <p>
+            Price: <span className="balance">{balance}</span>
+         </p>
+         <p>Id: {id}</p>
+         <button className="btn btn-primary">Add to Cart</button>
       </div>
    );
 };
